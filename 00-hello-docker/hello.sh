e78fc80ac8acdd744e32f32e1ab4b5df01fde9be
Akshar Patel <patel.akshar111@gmail.com>
2025-10-01 20:39:16 -0700

    lesson(00): first Dockerfile + hello script

--- 00-hello-docker/hello.sh
+++ 00-hello-docker/hello.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-echo "Hello from Docker Lab"
+# ^ Shebang: tells the OS to run this file with /bin/sh (Alpine uses BusyBox sh).
+
+echo "Hello from inside a container! $(date)"
+# ^ Prints a message + the current time from inside the container's environment.
